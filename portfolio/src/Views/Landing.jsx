4578bd36cab60e194a808e4cdcdee441867e8e97
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-04-14 03:17:38 -0300

    first commit

---
+++ portfolio/src/Views/Landing.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Menu from "../components/Menu/menu";
+
+const Landing = () => {
+  return (
+    <div>
+      <img src="" alt="mi foto" />
+      <img src="" alt="logo" />
+      <Menu />
+    </div>
+  );
+};
+
+export default Landing;
---
+++ portfolio/src/components/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div>
+      <div>Home</div>
+      <div>Projects</div>
+      <div>AboutMe</div>
+      <div>Contact</div>
+    </div>
+  );
+};
+
+export default Menu;
