7acacf3e0838ed656df0775fba4f2956109402d6
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-04-14 05:36:27 -0300

    mas estilos

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,14 +3,17 @@ import Landing from '../src/Views/Landing'
 import {theme} from './theme'
 
 function App() {
-
   return (
     <Box
-    backgroundImage={`linear-gradient(to bottom right, ${theme.colors.bg1}, ${theme.colors.bg2})`}
+      backgroundImage={`linear-gradient(to bottom right, ${theme.colors.bg1}, ${theme.colors.bg2})`}
       minHeight="100vh"
-      minW="100vh"
+      minW="100vw"
+      display="flex"
+      flexDirection="column"
     >
-      <Landing/>
+      <Box border={'1px solid black'} flex="1">
+        <Landing/>
+      </Box>
     </Box>
   )
 }
--- portfolio/src/Views/Landing.jsx
+++ portfolio/src/Views/Landing.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Menu from "../components/Menu/menu";
+import { Flex, Box } from "@chakra-ui/react";
 
 const Landing = () => {
   return (
-    <div>
-      <img src="" alt="mi foto" />
-      <img src="" alt="logo" />
-      <Menu />
-    </div>
+    <Flex height='100vh' justifyContent="space-evenly" alignItems="center">
+      <Box maxW="50%">
+            <img src="" alt="mi foto" />
+            <img src="" alt="logo" />
+            {/* aca irian un par de boludces sobre mi */}
+      </Box>
+      <Box maxW="50%">
+        <Menu />
+      </Box>
+    </Flex>
   );
 };
 
-export default Landing;
+export default Landing
