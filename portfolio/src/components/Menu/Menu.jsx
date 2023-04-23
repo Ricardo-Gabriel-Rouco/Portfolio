0bc83e74fafbd9a2813420e594b608452a0109cc
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-04-23 02:16:59 -0300

    agregado iconos

--- portfolio/src/components/Menu/Menu.jsx
+++ portfolio/src/components/Menu/Menu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Flex, Link } from "@chakra-ui/react";
+import { Flex, Link, Icon } from "@chakra-ui/react";
+import { GoHome, GoProject, GoBriefcase, GoMention } from "react-icons/go";
 import { theme } from "../../theme";
 import { Link as routerlink } from "react-router-dom";
 const MainMenu = () => {
@@ -10,6 +11,8 @@ const MainMenu = () => {
         marginBottom={"3rem"}
         fontSize={theme.fontSizes.lg}
       >
+        {/* link a la agina inicial, buscar icons de react icon o de chakra */}
+        <Icon as={GoHome} marginRight={"10px"} />
         Home
       </Link>
       <Link
@@ -19,6 +22,8 @@ const MainMenu = () => {
         as={routerlink}
         to="/projects"
       >
+        {/* listado de proyectos con sus respectivos links a github, call of whatever, rick and morty, pi, pf y pi mejorandose (no olvidarse de aclarar) */}
+        <Icon as={GoProject} marginRight={"10px"} />
         Projects
       </Link>
       <Link
@@ -26,6 +31,8 @@ const MainMenu = () => {
         marginBottom={"3rem"}
         fontSize={theme.fontSizes.lg}
       >
+        {/* aca pondre una suerte de CV con tecnologias, algunos datos personales y va ir mi foto de la landing */}
+        <Icon as={GoBriefcase} marginRight={"10px"} />
         AboutMe
       </Link>
       <Link
@@ -33,6 +40,8 @@ const MainMenu = () => {
         marginBottom={"3rem"}
         fontSize={theme.fontSizes.lg}
       >
+        {/* aca va air formulario de contacto via mail, ver email.js */}
+        <Icon as={GoMention} marginRight={"10px"} />
         Contact
       </Link>
     </Flex>
